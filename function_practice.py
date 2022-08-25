3389660d35520ffc5721f0aa44a8c598f4d4a82b
Augie Bren <Augie>
2022-08-25 15:36:52 -0500

    first commit

---
+++ function_practice.py
@@ -0,0 +1,22 @@
+def  hello():
+    print("Welcome to the function")
+
+def pack(item1, item2, item3):
+    pack_list = [item1, item2, item3]
+    print(pack_list)
+
+def eat_lunch(list):
+    if len(list) == 0:
+        print("My lunch box is empty")
+    else:
+        for i in range(len(list)):
+            if i == 0:
+                print(f"First I eat {list[0]}")
+            else:
+                print(f"Next I eat {list[i]}")
+        
+
+
+hello()
+pack("sandwhich", "soda", "Banana")
+eat_lunch(["sandwich", "chips", "banana", "chwey bar", "fruti snacks"])
